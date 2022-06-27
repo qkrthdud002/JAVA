8b4a3ecebdd851f2c2177af9a621c24f01fece51
qkrthdud002 <qkrthdud002@gmail.com>
2022-06-27 17:15:28 +0900

    JavaSubject

--- src/Subject03/game.java
+++ src/Subject03/game.java
@@ -9,16 +9,14 @@ public class game {
 
         int win=0, lose=0, draw=0;
 
-
-        //배열 index 시작 값이 0이어서서
-       String[] game = {"가위", "바위", "보"};
+        String[] game = {"가위", "바위", "보"};
 
         Scanner sc = new Scanner(System.in);
 
         while(true) {
-            double random = Math.random() * (game.length + 1);
-
+            double random = Math.random() * (game.length);
             int num = (int)random;
+
             System.out.println("** 가위바위보 가운데 하나를 입력하세요 **");
             System.out.println("--------------------------------------");
             System.out.print("(사용자) : ");
@@ -29,14 +27,17 @@ public class game {
                     System.out.println(user + " vs " + game[num]);
                     System.out.println("졌습니다.");
                     lose++;
+
                 } else if ((user.equals("가위") && game[num].equals("보")) || (user.equals("바위") && game[num].equals("가위")) || (user.equals("보") && game[num].equals("바위"))) {
                     System.out.println(user + " vs " + game[num]);
                     System.out.println("이겼습니다.");
                     win++;
+
                 } else if (user.equals(game[num])) {
                     System.out.println(user + " vs " + game[num]);
                     System.out.println("비겼습니다.");
                     draw++;
+
                 } else if (user.equals("quit")) {
                     break;
                 }
@@ -44,6 +45,7 @@ public class game {
                 System.out.println("====================================");
                 System.out.printf("전적 : %d %d %d\n", win, draw, lose);
                 System.out.println();
+
             }catch (ArrayIndexOutOfBoundsException e){
                 e.printStackTrace();
             }
